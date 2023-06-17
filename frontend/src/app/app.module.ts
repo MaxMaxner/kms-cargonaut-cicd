315fa7e197dd28042852d58c72e3d3ed4decba69
taliamaliacokg <talia.hortac@mni.thm.de>
2023-06-17 22:21:27 +0200

    Client implementations for Gesuch/Angebot1/Angebo2 are done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GesucheErstellenComponent } from './gesuche-erstellen/gesuche-erstellen.component';
-import { AngebotErstellen1Component } from './angebot-erstellen1/angebot-erstellen1.component';
-import { AngebotErstellen2Component } from './angebot-erstellen2/angebot-erstellen2.component';
+import { AngebotErstellenEinsComponent } from './angebot-erstellen1/angebot-erstellen1.component';
+import { AngebotErstellenZweiComponent } from './angebot-erstellen2/angebot-erstellen2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GesucheErstellenComponent,
-    AngebotErstellen1Component,
-    AngebotErstellen2Component
+    AngebotErstellenEinsComponent,
+    AngebotErstellenZweiComponent
   ],
   imports: [
     BrowserModule,
