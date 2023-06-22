609cc86bfb6f3a5019573b583b9ed0034e755b0b
Nadine Denkhaus <nadine.denkhaus@mni.thm.de>
2023-06-22 15:19:07 +0200

    add logic for differentiate between owner and customer

--- frontend/src/app/offer-details/offer-details.component.ts
+++ frontend/src/app/offer-details/offer-details.component.ts
@@ -8,5 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class OfferDetailsComponent {
 
   public owner  = true;
+  public booked = true;
+  public rideDone= true;
+  public allowTracking = true;
 
 }
