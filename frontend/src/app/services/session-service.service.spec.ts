83aa4cf987a3164a193051e46e3abb7f425341f4
Hendrik bruss <Hendrikbruss1999@.com>
2023-06-26 17:49:58 +0200

    fixed error in session service spec

--- frontend/src/app/services/session-service.service.spec.ts
+++ frontend/src/app/services/session-service.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SessionServiceService } from './session-service.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('SessionServiceService', () => {
   let service: SessionServiceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],});
+
     service = TestBed.inject(SessionServiceService);
   });
 
