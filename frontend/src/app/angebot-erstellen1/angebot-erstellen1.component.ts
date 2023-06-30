bf693cc4ce89427134f998e7ef289f6fa4fc584c
taliamaliacokg <talia.hortac@mni.thm.de>
2023-06-30 19:20:35 +0200

    Angebot Erstellen unit test and bug fixes

--- frontend/src/app/angebot-erstellen1/angebot-erstellen1.component.ts
+++ frontend/src/app/angebot-erstellen1/angebot-erstellen1.component.ts
@@ -9,6 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AngebotErstellenEinsComponent implements OnInit {
   myForm!: FormGroup;
+  responseData: any;
 
   constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) {}
 
@@ -30,48 +31,61 @@ export class AngebotErstellenEinsComponent implements OnInit {
     });
   }
 
-  validateForm(): void {
+  onSubmit(): void {
     if (this.myForm.invalid) {
-      const emptyInputs = Object.keys(this.myForm.controls).filter((key: string) => {
-        const control = this.myForm.get(key);
-        const isCheckbox = control?.value === false;
-        const isSonstigeInfos = key === 'sonstigeinfo' && control?.value === '';
-        const isZwischenziel = key === 'zwischenziel' && control?.value === '';
-
-        return control?.value === '' || isCheckbox || isSonstigeInfos || isZwischenziel;
-      });
-
-      if (emptyInputs.length > 0) {
-        const errorMessage = 'Bitte füllen Sie alle Felder aus: ' + emptyInputs.join(', ');
-        alert(errorMessage);
-        return;
-      }
+      this.validateForm();
+      return;
     }
 
     const formData: FormData = new FormData();
     formData.append('datum', this.myForm.get('datum')?.value);
-    formData.append('time', this.myForm.get('time')?.value);
-    formData.append('anmerkungen', this.myForm.get('anmerkungen')?.value);
-    formData.append('musik', this.myForm.get('musik')?.value);
-    formData.append('unterhaltung', this.myForm.get('unterhaltung')?.value);
-    formData.append('mitfahrer', this.myForm.get('mitfahrer')?.value);
+    formData.append('time', this.myForm.get('time')?.value || ''); // Set empty string if null or undefined
+    formData.append('anmerkungen', this.myForm.get('anmerkungen')?.value || '');
+    formData.append('musik', this.myForm.get('musik')?.value.toString());
+    formData.append('unterhaltung', this.myForm.get('unterhaltung')?.value.toString());
+    formData.append('mitfahrer', this.myForm.get('mitfahrer')?.value.toString());
     formData.append('sonstigeinfo', this.myForm.get('sonstigeinfo')?.value);
     formData.append('von', this.myForm.get('von')?.value);
     formData.append('nach', this.myForm.get('nach')?.value);
-    formData.append('zwischenziel', this.myForm.get('zwischenziel')?.value);
+    formData.append('zwischenziel', this.myForm.get('zwischenziel')?.value || '');
     formData.append('handynummer', this.myForm.get('handynummer')?.value);
-    formData.append('keineTiere', this.myForm.get('keineTiere')?.value);
-    formData.append('nichtraucher', this.myForm.get('nichtraucher')?.value);
+    formData.append('keineTiere', this.myForm.get('keineTiere')?.value.toString());
+    formData.append('nichtraucher', this.myForm.get('nichtraucher')?.value.toString());
 
-    this.httpClient.post('offerOne', formData).subscribe(
+    this.httpClient.post('http://localhost:3000/offerOne', formData).subscribe(
       (response) => {
         // Handle successful response
         console.log(response);
+        this.responseData = response;
+        this.resetForm(); // Reset the form after successful submission
       },
       (error) => {
         // Handle error
         console.error(error);
       }
     );
+  }
+
+  validateForm(): void {
+    const emptyInputs = Object.keys(this.myForm.controls).filter((key: string) => {
+      const control = this.myForm.get(key);
+      const isCheckbox = control?.value === false;
+      const isSonstigeInfos = key === 'sonstigeinfo' && control?.value === '';
+      const isZwischenziel = key === 'zwischenziel' && control?.value === '';
+
+      return !isCheckbox && !isSonstigeInfos && !isZwischenziel && control?.invalid;
+    });
+
+    console.log('Empty inputs:', emptyInputs);
+  }
+
+  resetForm(): void {
+    this.myForm.reset({
+      musik: false,
+      unterhaltung: false,
+      mitfahrer: false,
+      keineTiere: false,
+      nichtraucher: false
+    });
   }
 }
