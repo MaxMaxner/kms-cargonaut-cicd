714d23066977ddba53740cc0b8bfa67649e2a007
Clemens Weber <jan.clemens.weber-2@mni.thm.de>
2023-06-30 16:59:17 +0200

    v0.0.1

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { User2Service } from '../services/user2.service';
+import { Component, OnInit } from '@angular/core'
+import { User2Service } from '../services/user2.service'
+import { IUser2 } from 'src/interfaces/IUser2'
 
 @Component({
     selector: 'app-profile',
@@ -9,21 +10,21 @@ import { User2Service } from '../services/user2.service';
 export class ProfileComponent implements OnInit {
     constructor(private user2Service: User2Service) {}
 
-    profilePicture = '';
-    firstname: string = '';
-    lastname: string = '';
-    email: string = '';
-    age: number = 0;
-    birthday: string = '';
-    birthdate: Date = new Date();
-    notice: string = '';
-    phone: string = '';
-    reviews: string[] = [];
+    profilePicture = ''
+    firstname: string = ''
+    lastname: string = ''
+    email: string = ''
+    age: number = 0
+    birthday: string = ''
+    birthdate: Date = new Date()
+    notice: string = ''
+    phone: string = ''
+    reviews: string[] = []
 
-    drivenWeight: number = 0;
-    drivenPeople: number = 0;
-    drivenKilometers: number = 0;
-    smokingStatus: string = '';
+    drivenWeight: number = 0
+    drivenPeople: number = 0
+    drivenKilometers: number = 0
+    smokingStatus: string = ''
     languageOptions: string[] = [
         'Englisch',
         'Spanisch',
@@ -31,65 +32,66 @@ export class ProfileComponent implements OnInit {
         'Deutsch',
         'Chinesisch',
         'Japanisch',
-    ];
-    spokenLanguages: string[] = [];
+    ]
+    spokenLanguages: string[] = []
 
-    editingMode: boolean = false;
+    editingMode: boolean = false
 
     ngOnInit(): void {
         // @TODO: Get id from url and fetch data from backend
         // Initialize profile data
-        this.profilePicture = 'assets/img/profile.png';
-        this.spokenLanguages = ['Deutsch'];
-        this.drivenKilometers = 4500;
-        this.drivenPeople = 10;
-        this.drivenWeight = 378;
+        this.profilePicture = 'assets/img/profile.png'
+        this.firstname = 'Manfred'
+        this.lastname = 'Degenhort'
+        this.email = 'manfred1965@outlook.com'
+        this.birthday = '1965-02-01'
+        this.birthdate = new Date(this.birthday)
+        this.phone = '+49 0127893793'
+        this.spokenLanguages = ['Deutsch']
+        this.drivenKilometers = 4500
+        this.drivenPeople = 10
+        this.drivenWeight = 378
+        this.smokingStatus = 'Raucher'
         this.notice =
-            'Hallo! Willkommen auf meinem Profil. Ich bin der Kapitän der Landstraße! Folgt mit für Spaß und Kilometer!';
+            'Hallo! Willkommen auf meinem Profil. Ich bin der Kapitän der Landstraße! Folgt mit für Spaß und Kilometer!'
         this.reviews = [
             'Great person to work with!',
             'Highly recommended!',
             'Very talented individual.',
-        ];
-        this.displayUser();
+        ]
+        this.displayUser()
     }
 
     async displayUser() {
-        let user = this.user2Service.getUser('admin@admin.de');
-        console.log(user);
-        this.email = (await user).mail;
-        this.firstname = (await user).firstname;
-        this.lastname = (await user).lastname;
-        this.smokingStatus = (await user).smocker == 1 ? 'Raucher' : 'Nicht-Raucher';
-        this.birthdate = (await user).birthday;
-        this.phone = (await user).mobilephone;
+        let user
+        await this.user2Service.getUser('admin@admin.de')
     }
 
     toggleEditingMode(): void {
-        this.editingMode = !this.editingMode;
+        this.editingMode = !this.editingMode
     }
 
     saveChanges(): void {
-        this.editingMode = false;
+        this.editingMode = false
     }
 
     calculateAge() {
-        const today: Date = new Date();
-        const birthdate: Date = new Date(this.birthday);
-        this.age = today.getFullYear() - birthdate.getFullYear();
+        const today: Date = new Date()
+        const birthdate: Date = new Date(this.birthday)
+        this.age = today.getFullYear() - birthdate.getFullYear()
 
         const hasNotHadBirthdayYet: boolean =
             today.getMonth() < birthdate.getMonth() ||
-            (today.getMonth() === birthdate.getMonth() && today.getDate() < birthdate.getDate());
+            (today.getMonth() === birthdate.getMonth() && today.getDate() < birthdate.getDate())
 
         if (hasNotHadBirthdayYet) {
-            this.age--;
+            this.age--
         }
 
-        return this.age;
+        return this.age
     }
 
     toggleLanguage(languages: string[]): void {
-        this.spokenLanguages = languages;
+        this.spokenLanguages = languages
     }
 }
--- frontend/src/app/services/user2.service.ts
+++ frontend/src/app/services/user2.service.ts
@@ -1,12 +1,10 @@
-import { Injectable } from '@angular/core';
-import { IUser2 } from '../../interfaces/IUser2';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core'
+import { IUser2 } from '../../interfaces/IUser2'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-};
-
-let user: IUser2;
+}
 
 @Injectable({
     providedIn: 'root',
@@ -14,24 +12,18 @@ let user: IUser2;
 export class User2Service {
     public environment = {
         apiUrl: 'http://localhost:8080',
-    };
+    }
 
     constructor(private http: HttpClient) {}
 
-    getUserFromDB(mail: string): Promise<void | IUser2> {
-        console.log('working Service on: ', this.environment.apiUrl + '/user/' + mail);
+    getUser(mail: string): Promise<void> {
+        console.log('working Service on: ', this.environment.apiUrl + '/' + mail)
 
         return this.http
-            .get(this.environment.apiUrl + '/user/' + mail, httpOptions)
+            .get(`${this.environment.apiUrl}/user/` + mail, httpOptions)
             .toPromise()
             .then((res: any) => {
-                user = res.user;
-                console.log(res);
-            });
-    }
-
-    async getUser(mail: string): Promise<IUser2> {
-        await this.getUserFromDB(mail);
-        return user;
+                user: res.user
+            })
     }
 }
