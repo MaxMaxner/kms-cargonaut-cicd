729bdb6f6f05dcff2b38d10c97248e0abcb97818
Clemens Weber <jan.clemens.weber-2@mni.thm.de>
2023-07-02 19:49:47 +0200

    fix tests

--- frontend/src/app/profile/profile.component.spec.ts
+++ frontend/src/app/profile/profile.component.spec.ts
@@ -1,21 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProfileComponent } from './profile.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProfileComponent', () => {
-  let component: ProfileComponent;
-  let fixture: ComponentFixture<ProfileComponent>;
+    let component: ProfileComponent;
+    let fixture: ComponentFixture<ProfileComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ProfileComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ProfileComponent],
+            imports: [HttpClientTestingModule],
+        });
+        fixture = TestBed.createComponent(ProfileComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(ProfileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- frontend/src/app/services/vehicle.service.spec.ts
+++ frontend/src/app/services/vehicle.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { VehicleService } from './vehicle.service';
 
 describe('VehicleService', () => {
-  let service: VehicleService;
+    let service: VehicleService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(VehicleService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        });
+        service = TestBed.inject(VehicleService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
